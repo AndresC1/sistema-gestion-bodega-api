1c28cfe31208332b6749eeca94e7a0b71b632587
AndresC1 <andrescornejolira@gmail.com>
2023-08-14 16:21:20 -0600

    Creacion de seeder para proveedores

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             SectorSeeder::class,
             OrganizationSeeder::class,
             UserSeeder::class,
+            ProviderSeeder::class,
         ]);
     }
 }
