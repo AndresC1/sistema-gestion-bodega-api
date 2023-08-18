7c0c4a697a9bab9149b91173dec028e31a18a9dd
AndresC1 <andrescornejolira@gmail.com>
2023-08-17 18:19:24 -0600

    Creacion de endpoint para buscar producto y agregar producto

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\Product\ProductCleanResource;
+use Exception;
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+        try{
+            $product = Product::create($request->validated());
+            return response()->json([
+                'producto' => new ProductCleanResource($product),
+                'mensaje' => 'Producto creado correctamente',
+                'estado' => 201
+            ], 201);
+        } catch (Exception $e){
+            return response()->json([
+                'mensaje' => 'Error al crear el producto',
+                'error' => $e->getMessage(),
+                'estado' => 500
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+
+    public function search(string $name)
+    {
+        try{
+            $products = Product::where('name', 'like', "%$name%")->get();
+            return response()->json(ProductCleanResource::collection($products), 200);
+        } catch(Exception $e){
+            return response()->json([
+                'mensaje' => 'Error al buscar el producto',
+                'error' => $e->getMessage(),
+                'estado' => 500
+            ], 500);
+        }
+    }
+}
---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255|unique:products,name',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'El nombre del producto es requerido',
+            'name.string' => 'El nombre del producto debe ser una cadena de caracteres',
+            'name.max' => 'El nombre del producto debe tener máximo 255 caracteres',
+            'name.unique' => 'El nombre del producto ya existe',
+        ];
+    }
+}
