bb2489fd7a684cfd03734fc3ede75f0836f6ad5a
AndresC1 <andrescornejolira@gmail.com>
2023-08-25 13:36:08 -0600

    Muestra de entradas y salidas del inventario

--- routes/api.php
+++ routes/api.php
@@ -139,7 +139,7 @@ Route::prefix('v1')->group(function () {
 
         // Inventarios
         Route::middleware('check_permission:view_inventory')->group(function () {
-            Route::get('/inventory', [InventoryController::class, "index"]);
+            Route::apiResource('/inventory', InventoryController::class)->only(['index', 'show']);
             Route::prefix('inventario')->group(function () {
                 Route::get('/min-stock', [InventoryController::class, "list_min_stock"]);
             });
