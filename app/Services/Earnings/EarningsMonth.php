e3aeb6a2e06954d182611e07db8f39a7c758f9d1
AndresC1 <andrescornejolira@gmail.com>
2023-09-16 22:59:26 -0600

    Endpoint para ganancias (por dia, semana y mes)

---
+++ app/Services/Earnings/EarningsMonth.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services\Earnings;
+
+use App\Models\Sale;
+
+class EarningsMonth
+{
+    public function calculate()
+    {
+        $total = 0;
+        $sales = 0;
+        $listSales = Sale::where('organization_id', auth()->user()->organization_id)
+            ->where('date', '>=', now('America/Managua')->subDays(30))
+            ->get();
+        foreach ($listSales as $sale) {
+            $total += $sale->earning_total;
+            $sales++;
+        }
+        return [
+            'earnings_total' => $total,
+            'sales_total' => $sales,
+            'range' => [
+                'start' => now('America/Managua')->subDays(30)->format('d/m/Y'),
+                'end' => now('America/Managua')->format('d/m/Y')
+            ]
+        ];
+    }
+}
